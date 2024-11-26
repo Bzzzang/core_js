327290881d316d933af7c820101d5fe5a2c005e0
Bzzzang <sindmsql@gmail.com>
2024-11-26 14:44:25 +0900

    Shadow

--- client/main.js
+++ client/main.js
@@ -1,46 +1,43 @@
-
-
-import { getNode, setStorage, getStorage, deleteStorage } from "./lib/index.js";
-
-
-const textField = getNode('#textField');
-const clearButton = getNode('button[data-name="clear"]');
-
-
-// 1. 인풋 이벤트 바인딩
-//   - 인풋(textarea) 태그 선택 
-//   - addEventListener('input',handler)
-//   - handler 함수 안에서 값 가져오기 (this.value)
-
-// 2. 인풋 값을 로컬 스토리지에 저장(타이핑 하는 순간 순간마다)
-//   - setStorage(key,value)
-
-// 3. init 함수 안에서 로컬스토리지에 있는 값을 가져와 인풋의 value로 설정
-//   - getStorage
-//   - text.value = value
-
-// 4. 새로고침 => 데이터 유지 
-
-// 5. clear 버튼 클릭시 데이터 제거 (로컬스토리지, 인풋값)
+// console.log(temp.content.cloneNode(true));
 
 
 
-function handleInput(){
-  const value = this.value;
-  setStorage('text',value);
+class MyElement extends HTMLElement {
+
+  constructor(){
+    super();
+
+    this.attachShadow({mode:'open'});
+    this._render();
+    this.card = this.shadowRoot.querySelector('.card')
+  }
+
+  connectedCallback(){
+    this.card.addEventListener('click',this.handleClick.bind(this))
+  }
+
+  handleClick(){
+    console.log(this.getAttribute('data-name'));
+  }
+
+  _render(){
+      this.shadowRoot.innerHTML = `
+      
+        <style>
+        @import url('./style.css'); 
+        </style>
+
+        
+        <div class="card">
+          <slot name="title">Default Title</slot>
+          <slot name="content">Default Contents</slot>
+          <slot></slot>
+        </div>
+    `
+  }
+
 }
 
-function handleClear(){
-  textField.value = '';
-  deleteStorage('text')
-}
 
-function init(){
-  getStorage('text')
-    .then((res)=> textField.value = res)
-}
 
-textField.addEventListener('input',handleInput)
-clearButton.addEventListener('click',handleClear)
-
-init();
+customElements.define('my-element',MyElement);
