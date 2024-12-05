adc2c30f00baa10a6910c652a5128dcc189fa087
Bzzzang <sindmsql@gmail.com>
2024-12-05 17:12:30 +0900

    12/5

--- src/Layout/Header.ts
+++ src/Layout/Header.ts
@@ -1,9 +1,17 @@
+
 import { LitElement, html, css, CSSResultGroup } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import resetCSS from "./resetCSS";
+import { Auth } from './../@types/type';
+import Swal from 'sweetalert2';
+import pb from "../api/pocketbase";
+
 
 @customElement("c-header")
 class Header extends LitElement {
+
+  @state() private loginData:Auth = {} as Auth
+
   static styles: CSSResultGroup = [
     resetCSS,
     css`
@@ -33,7 +41,41 @@ class Header extends LitElement {
     `,
   ];
 
+  connectedCallback(){
+    super.connectedCallback();
+    this.fetchData();
+  }
+
+  fetchData(){
+    const auth = JSON.parse(localStorage.getItem('auth') ?? "{}")
+    
+    this.loginData = auth;
+  }
+
+  handleLogout(e:Event){
+    e.preventDefault()
+    Swal.fire({
+      title: '로그아웃',
+      text: '정말로 로그아웃 하시겠습니까?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: '네',
+      cancelButtonText: '아니요',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem('auth');
+        pb.authStore.clear();
+        // this.loginData.isAuth = false;
+        // this.requestUpdate()
+        location.reload();
+      }
+    });
+  }
+
   render() {
+
+    const {isAuth, user} = this.loginData
+
     return html`
       <header>
         <h1 class="logo">
@@ -43,9 +85,20 @@ class Header extends LitElement {
         <nav>
           <ul>
             <li><a href="/">About</a></li>
-            <li><a href="/">Product</a></li>
+            <li><a href="/src/pages/product/">Product</a></li>
             <li><a href="/">Contact</a></li>
-            <li><a href="/">Login</a></li>
+            <li>
+            ${
+              !isAuth
+              ? html`<a href="/src/pages/login/">Login</a>`
+              : html`
+              <div>
+                <span>${user.name}님</span>
+                <a href="/" @click=${this.handleLogout}>Logout</a>
+              </div>
+             `
+            }
+            </li>
           </ul>
         </nav>
       </header>
---
+++ src/components/Login.ts
@@ -0,0 +1,105 @@
+import { LitElement, html, css, CSSResultGroup } from "lit";
+import { customElement } from "lit/decorators.js";
+import resetCSS from "../Layout/resetCSS";
+import pb from "../api/pocketbase";
+import Swal from "sweetalert2";
+
+@customElement("login-element")
+class Login extends LitElement {
+
+  static styles:CSSResultGroup = [
+    resetCSS,
+    css`
+    .container{
+      max-width: 400px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-50%);
+      text-align: center;
+    
+      & h1 {
+        font-size: 3rem;
+        font-weight: bold;
+      }
+    
+      & hr{
+        margin: 2rem 0 ;
+        height: 4px;
+        background-color: white;
+      }
+    
+      & form{
+        margin-bottom: 5rem;
+    
+        & input{
+          box-sizing: border-box;
+          border: 1px solid rgb(122, 122, 122);
+          padding: 1rem;
+          min-width: 300px;
+          margin: 0.2rem 0;
+          outline: none;
+    
+          &:focus{
+            border: 1px solid hotpink;
+          }
+        }
+    
+        & button[type="submit"]{
+          margin-top: 1.5rem;
+          border: none;
+          background: pink;
+          color: #242424;
+          padding: 1rem;
+          cursor:pointer;
+          width: 100%;
+        }
+      }
+    
+    }
+    `
+  ]
+
+  get idInput(){
+    return this.renderRoot.querySelector<HTMLInputElement>('#idField')!
+  }
+
+  get pwInput(){
+    return this.renderRoot.querySelector<HTMLInputElement>('#pwField')!
+  }
+
+  async fetchData(){
+    const id = 'sindmsql@gmail.com';
+    const pw = 'dmsql123';
+    
+    await pb.collection('users').authWithPassword(id,pw)
+  }
+
+  handleLogin(e:Event){
+    e.preventDefault();
+    this.fetchData();
+    
+  }
+
+  render() {
+    return html`
+      <div class="container">
+        <h1>로그인</h1>
+        <hr />
+        <form>
+          <div>
+            <label for="idField"></label>
+            <input type="email" id="idField" placeholder="아이디(이메일)" />
+          </div>
+          <div>
+            <label for="pwField"></label>
+            <input type="password" id="pwField" placeholder="비밀번호" />
+          </div>
+          <button type="submit" class="login">LOGIN</button>
+        </form>
+
+        <a class="register" href="/src/pages/register/">간편 회원가입</a>
+      </div>
+    `
+  }
+}
